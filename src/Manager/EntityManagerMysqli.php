399910e63de0769ad4c2cb86e4a401ec19ed9db0
Azulis <micka@azulis.be>
2015-08-01 19:19:34 +0200

    MonoKit update

--- src/Manager/EntityManagerMysqli.php
+++ src/Manager/EntityManagerMysqli.php
@@ -56,28 +56,35 @@ Abstract Class EntityManagerMysqli extends EntityManager
 
     /**
      * @param SqlInterface $sql
-     * @param string $entityClass
      * @return EntityManagerMysqli
      * @throws SqlException
      */
-	protected function query( SqlInterface $sql , $entityClass = null )
+	protected function query( SqlInterface $sql )
 	{
         $this->setSql( $sql );
-
-        $this->removeAll();
 
 		if ( !$this->mysqli_result = $this->mysqli->query( $this->getSql()->toString() ) )
 			throw new SqlException( SqlException::ERROR_SQL , $this , $this->getSql()->toString() );
 
+		return $this;
+	}
+
+    /**
+     * @param string $entityClass
+     * @return EntityManagerMysqli
+     */
+    protected function fetchAll( $entityClass )
+    {
+        $this->removeAll();
+
         if ( $this->mysqli->affected_rows === 0 )
             return $this;
 
-        if ( !is_null( $entityClass ) )
-            while( $data = $this->mysqli_result->fetch_assoc() )
-                $this->add( new $entityClass() , $data );
+        while( $data = $this->mysqli_result->fetch_assoc() )
+            $this->add( new $entityClass() , $data );
 
-		return $this;
-	}
+        return $this;
+    }
 
 	/**
 	* @return int
