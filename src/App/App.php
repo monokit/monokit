f0d43aa4bee6dabf0e5ae1a76f5566f79efd33ba
Azulis <micka@azulis.be>
2015-11-15 00:09:15 +0100

    MonoKit Update

--- src/App/App.php
+++ src/App/App.php
@@ -2,6 +2,7 @@
 
 namespace MonoKit\App;
 
+use MonoKit\File\File;
 use MonoKit\View\View;
 use MonoKit\Http\Dispatcher;
 use MonoKit\Http\UrlRequestDiscover;
@@ -10,6 +11,28 @@ use MonoKit\Foundation\Foundation;
 
 Abstract Class App extends Foundation
 {
+    /**
+     * @param $iniFile
+     * @return App
+     * @throws \MonoKit\File\FileException
+     */
+    public function setAppRegistryFromIniFile( $iniFile )
+    {
+        $this->AppRegistry()->setFromIniFile( $iniFile );
+        return $this;
+    }
+
+    /**
+     * @param string $iniFile
+     * @return App
+     * @throws \MonoKit\File\FileException
+     */
+    public function setAppRouterFromIniFile( $iniFile )
+    {
+        $this->AppRouter()->setFromIniFile( $iniFile );
+        return $this;
+    }
+
     /**
      * @param null $fileView
      * @param null $data
@@ -35,25 +58,35 @@ Abstract Class App extends Foundation
     }
 
     /**
-     * @param $iniFile
-     * @return App
-     * @throws \MonoKit\File\FileException
+     * @param $fileUrl
+     * @return bool|string
      */
-    public function setAppRegistryFromIniFile( $iniFile )
+    public function renderAsset( $fileUrl )
     {
-        $this->AppRegistry()->setFromIniFile( $iniFile );
-        return $this;
-    }
+        $fileUrl = ltrim( $fileUrl , "/" );
 
-    /**
-     * @param string $iniFile
-     * @return App
-     * @throws \MonoKit\File\FileException
-     */
-    public function setAppRouterFromIniFile( $iniFile )
-    {
-        $this->AppRouter()->setFromIniFile( $iniFile );
-        return $this;
+        preg_match('/@(?P<AppName>\w+)\//', $fileUrl , $matches );
+
+        if ( isset( $matches["AppName"] ) )
+        {
+            $urlArray = explode("@{$matches['AppName']}/" , $fileUrl , 2 );
+            $url = "../../{$matches['AppName']}/Assets/{$urlArray[1]}";
+        } else {
+            $url = __DIR__ . "/Assets/" . $fileUrl;
+        }
+
+        $File = new File( $url );
+
+        if ( $File->isFile() )
+        {
+            header( "Content-type:".$File->getMimeType() );
+            echo $File->getFileContent();
+            exit();
+        } else {
+            header("HTTP/1.0 404 Not Found");
+        }
+
+        return false;
     }
 
 }
