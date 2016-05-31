5ddc2c8fd0fc1fc4513624675710b1fcb441fdb0
Azulis <micka@azulis.be>
2016-06-01 01:54:28 +0200

    MonoKit Update

--- src/Http/Response/ResponseJson.php
+++ src/Http/Response/ResponseJson.php
@@ -2,8 +2,20 @@
 
 namespace MonoKit\Http\Response;
 
+use MonoKit\Foundation\Interfaces\JsonInterface;
+
 Class ResponseJson extends Response
 {
     const RESPONSE_HEADER = "Content-Type: application/json";
-    
+
+    /**
+     * ResponseJson constructor.
+     * @param JsonInterface|null $content
+     * @param int $status
+     */
+    public function __construct( JsonInterface $content = null , $status = 200 )
+    {
+        parent::__construct( $content->toJson() , $status );
+    }
+
 }
