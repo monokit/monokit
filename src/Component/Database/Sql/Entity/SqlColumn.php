6b6af9047e682271e3d5f8fa57f2de43d98e074a
Azulis <micka@azulis.be>
2018-06-11 15:49:29 +0200

    MonoKit Update

--- src/Component/Database/Sql/Entity/SqlColumn.php
+++ src/Component/Database/Sql/Entity/SqlColumn.php
@@ -45,11 +45,11 @@ class SqlColumn extends Column implements StringInterface
         if ( is_bool( $value ) )
             $value = ( $value === true ) ? "true" : "false";
 
-        if ( is_string( $value ) )
-            $value = "'{$value}'";
-
         if ( is_array( $value ) )
             $value = @implode( "," , $value );
+
+        if ( is_string( $value ) )
+            $value = "'{$value}'";
 
         $this->value = $value;
         return $this;
