3f4563ec6ae6c3f00260b48df4e531a72f30817e
Azulis <micka@azulis.be>
2018-06-12 23:20:40 +0200

    MonoKit Update

--- src/Foundation/Exception/ExceptionView.php
+++ src/Foundation/Exception/ExceptionView.php
@@ -5,18 +5,15 @@
     <meta charset="utf-8">
     <meta name="robots" content="noindex, nofollow">
 
-    <link rel="stylesheet" href="https://bootswatch.com/slate/bootstrap.min.css">
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
 
 </head>
-<body style="padding: 50px;">
+<body>
 
-    <div class="jumbotron" style="padding: 50px;">
+    <div class="jumbotron">
         <h1>MonoKit</h1>
         <p><?php echo $this->message; ?></p>
-        <p><a class="btn btn-primary btn-lg"><?php echo $this->class; ?></a></p>
+        <button class="btn btn-primary btn-lg"><?php echo $this->class; ?></button>
     </div>
 
     <div class="well well-lg"><?php echo $this->vars; ?></div>
